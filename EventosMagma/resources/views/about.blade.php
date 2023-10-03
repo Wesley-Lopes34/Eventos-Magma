970ff611f3b84f31dafb99da15182d0a3de4ae06
Wesley Lopes <mikemagma666@gmail.com>
2023-10-03 16:25:04 -0300

    uma implementada no projeto
    Fazendo algumas pastas de teste para ir pegando o jeito do Laravel

---
+++ EventosMagma/resources/views/about.blade.php
@@ -0,0 +1,11 @@
+@extends('layots.main')
+
+@section('title', 'EventosMagma')
+
+<h1>
+    Pagina de teste por enquanto
+</h1>
+
+@section('content')
+
+@endsection
---
+++ EventosMagma/resources/views/layots/main.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>@yield('title')</title>
+
+        <!-- Fonts do Google -->
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
+
+        <!-- CSS boostrap -->
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+        <!-- CSS da aplicação -->
+        
+
+    </head>
+    <body>
+        @yield('content')
+        <footer>
+            <p>
+                Eventos Magma &copy; 2023
+            </p>
+        </footer>
+    </body>
+</html>
