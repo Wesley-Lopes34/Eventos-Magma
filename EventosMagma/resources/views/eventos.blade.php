d60c80b25bbefafd4d0e19b356b06660615fd007
Wesley Lopes <mikemagma666@gmail.com>
2023-10-09 16:28:34 -0300

    implementada legal ou n

---
+++ EventosMagma/resources/views/eventos.blade.php
@@ -0,0 +1,16 @@
+@extends('layots.main')
+
+@section('title', 'Sobre')
+
+<p>
+    Aqui vai ficar parte para mostrar os eventos
+</p>
+
+<p>
+    Você esta agora mesmo na parte: {{$id}}
+</p>
+
+@section('content')
+
+
+@endsection
